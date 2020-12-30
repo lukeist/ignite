cd2acf11d6442bd9c25d049b38753227b31bc8c0
keyysa <apt.muenchen@gmail.com>
2020-12-30 15:16:40 -0500

    5. 6. Fetching Data

--- src/reducers/gamesReducer.js
+++ src/reducers/gamesReducer.js
@@ -10,12 +10,15 @@ const initState = {
 const gamesReducer = (state = initState, action) => {
   switch (action.type) {
     case "FETCH_GAMES":
-      return { ...state };
+      return {
+        ...state,
+        popular: action.payload.popular,
+        newGames: action.payload.newGames,
+        upcoming: action.payload.upcoming,
+      };
     default:
       return { ...state };
   }
 };
-
-// Action
 
 export default gamesReducer;
